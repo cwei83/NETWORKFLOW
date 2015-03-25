ad62e6459fb2a6cedbe1163992e4d9bf3e6f73e2
cwei <c.weiii94@gmail.com>
2015-03-25 16:56:39 +0800

    add PHILADELPHIA_NETWORK AND MODIFY DIJKSTRA

--- DIJKSTRA/PHI-DIJKSTRA.c
+++ DIJKSTRA/PHI-DIJKSTRA.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <limits.h>
-#define num_node 8
+#define num_node 13390
 
 typedef struct adlist *adPointer;
 typedef struct adlist {
@@ -21,7 +21,7 @@ element pop();
 
 void addList(int i, int j, int n);
 void write(int pre[], int dis[]);
-void dijkstra(int vertex);
+void dijkstra(int vertex, int goal);
 
 adPointer graph[num_node+1];
 element heap[num_node];
@@ -32,41 +32,31 @@ int main() {
 
     FILE *fin;
     char input;
-    int n=0, i=0, j=1, root=1;
+    int n=0, i=0, j=1, root=1, goal;
+    int x, y, z, check=0;
 
     for(int x=1;x<num_node+1;x++)
 	graph[x]=NULL;
 
-    fin = fopen("input.txt", "r");
-    input=fgetc(fin);
+    fin = fopen("Philadelphia_network.txt", "r");
 
-    while(1) {
-	if(input<='9'&&input>='0') {
-	    n*=10;
-	    n+=input-'0';
-	}
-	if(input==' ') {
-	    if(n>0)
-		addList(i+1,j,n);
-	    n=0; i++;
-	}
-	if(input=='\n') {
-	    if(n>0) {
-		//printf("i=%d j=%d n=%3d\n", i, j, n);
-		//				printf("\n");
-		addList(i+1,j,n);
-	    }
-	    i++; j++;
-	    if(i==j) 
-		break;
-	    n=0; i=0;
-	}
-	input=fgetc(fin);
+    while(fscanf(fin, "%d %d %d", &x, &y, &z)) {
+	addList(y, x, z);
+	if(check==0 && x==13389)
+	    check=1;
+	if(check==1)
+	    break;
     }
 
     fclose(fin);
 
-    dijkstra(1);
+//    printf("Enter your root: ");
+//    scanf("%d", &root);
+//    printf("Enter your goal: ");
+//    scanf("%d", &goal);
+
+    root=1; goal=1;
+    dijkstra(root, goal);
 
     return 0;
 
@@ -116,7 +106,7 @@ element pop() {
 }
 
 void push(element item) {
-    int i = ++heap_index;
+    int i=++heap_index;
 
     while(i!=1&&item.weight<heap[i/2].weight) {
 	heap[i] = heap[i/2];
@@ -126,12 +116,12 @@ void push(element item) {
     heap[i] = item;
 }
 
-void dijkstra(int vertex) {
+void dijkstra(int vertex, int goal) {
     int distance[num_node+1], pre[num_node+1];
     bool visit[num_node+1];
     element temp, top;
     adPointer ptr;
-    int current_vertex, current_weight;
+    int current_vertex, current_weight, temp_pre;
 
     for(int i=0;i<num_node+1;i++) {
 	distance[i] = INT_MAX;
@@ -150,25 +140,36 @@ void dijkstra(int vertex) {
 	current_vertex = top.vertex;
 	current_weight = top.weight;
 	visit[current_vertex]=1;
-	write(pre, distance);
+//	write(pre, distance);
 
 	for(ptr=graph[current_vertex];ptr;ptr=ptr->link) 
-	    if(visit[ptr->vertex]==0) 
-		//printf("ptr->vertex=%d\n", ptr->vertex);
+	    if(visit[ptr->vertex]==0) {
+//		printf("ptr->vertex=%d\n", ptr->vertex);
 		if(distance[current_vertex]+ptr->weight<distance[ptr->vertex]) {
 		    distance[ptr->vertex]=distance[current_vertex]+ptr->weight;
 		    //printf("distance[%d]=%d distance[%d]=%d\n", current_vertex, distance[current_vertex], ptr->vertex, distance[ptr->vertex]);
 		    //printf("distance[%d]+%d<distance[%d]\n", current_vertex, ptr->weight, ptr->vertex);
 		    pre[ptr->vertex]=current_vertex;
 		
-		    temp.vertex = ptr->vertex;
-		    temp.weight = ptr->weight;
-		    push(temp);
+		temp.vertex = ptr->vertex;
+		temp.weight = distance[ptr->vertex];
+		push(temp);
 		}
+	    }
     }
 
-    //for(int i=1;i<num_node+1;i++) 
-	//printf("distance[%d] = %d\n", i, distance[i]);
+    for(int i=1;i<num_node+1;i++) 
+	printf("distance[%d] = %d\n", i, distance[i]);
+/*	
+    printf("distance= %d\n", distance[goal]);
+    temp_pre = pre[goal];
+    printf("%3d", goal);
+    while(pre[temp_pre]!=0) {
+	printf("%8d", temp_pre);
+	temp_pre = pre[temp_pre];
+    }
+    printf("\n");
+*/
 }
 
 void write(int pre[], int dis[]) {
